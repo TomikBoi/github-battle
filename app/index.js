bf74bcdac39c11cc3998ca32262ad161254365fe
TomikBoi <toms.kedo@gmail.com>
2020-11-05 20:18:30 +0200

    Initial styling for popular nav bar

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom' //Allows render element to DOM
+import Popular from './components/Popular'
 import './index.css'
 
 //Component
@@ -7,14 +8,60 @@ import './index.css'
 //-Lifecycle
 //-UI
 
+// function isAuthed() {
+//   return true
+// }
+// function isNew() {
+//   return true
+// }
+
+// function showWarning() {
+//   return true
+// }
+
 // Define component, App is the name
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
-     return (
-       <div>
-         Hello Tom!
-       </div>
-     )
+
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    )
+
+
+
+
+    // const authed = isAuthed()
+    // const firstLogin = isNew()
+
+    // return (
+    //   <div>
+    //     <h1>👀</h1>
+    //     {showWarning() === true
+    //       ? <h3>🚨</h3>
+    //       : null}
+    //   </div>
+    // )
+
+    // if(firstLogin === true) {
+    //   return <h1>Welcome!</h1>
+    // } else if (authed === true) {
+    //   return <h1>Welcome back!</h1>
+    // } else {
+    //   return <h1>Loign to see your dashboard</h1>
+    // }
+
+    // const name = 'Tom'
+
+    //  return (
+    //    <React.Fragment>
+    //       <h1>Hello, {name}</h1>
+    //       <p>Today is {new Date().toLocaleString()}</p>
+    //       <p>What is 2 + 2? {2 + 2}</p>
+    //    </React.Fragment>
+    //  )
+
   }
 }
 
